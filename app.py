5db68518412828f34bd433f9cc4c4085483cea93
Bhuvaneshwaran <cselakshmanan@gmail.com>
2021-11-25 17:42:12 +0530

    changed app

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
-from flask import Flask, render_template, request, jsonify
-import os
-import numpy as np
-from prediction_service import prediction
+from flask import Flask
 
-webapp_root = "webapp"
+app = Flask(__name__)
 
-static_dir = os.path.join(webapp_root, "static")
-template_dir = os.path.join(webapp_root, "templates")
-
-app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    return "hello world!"
+    return "Hello, world!"
+
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=5000, debug=True)
     app.run()
