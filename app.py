6d7d4be4e843ef4d1106b20caee3aaeacd650d3d
Bhuvaneshwaran <cselakshmanan@gmail.com>
2021-11-25 17:35:42 +0530

    initial commit

--- app.py
+++ app.py
@@ -12,25 +12,7 @@ app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        try:
-            if request.form:
-                dict_req = dict(request.form)
-                print("coming here in form", dict_req)
-                response = prediction.form_response(dict_req)
-                return render_template("index.html", response=response)
-            elif request.json:
-                print("coming here in json")
-                response = prediction.api_response(request.json)
-                return jsonify(response)
-        except Exception as e:
-            print(e)
-            error = {"error": "Something went wrong!! Try again later!"}
-            error = {"error": e}
-            return render_template("404.html", error=error)
-
-    else:
-        return render_template("index.html")
+    return "hello world"
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=5000, debug=True)
